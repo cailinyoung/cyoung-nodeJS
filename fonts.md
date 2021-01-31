a18dd40c4165324216b38c498a2498761c1f60d6
Cailin Young <cailinfayyoung@gmail.com>
2021-01-30 18:13:19 -0700

    modifying files

---
+++ fonts.md
@@ -0,0 +1,17 @@
+<a href="https://github.com/powerline" style="float:right"><img src="https://avatars2.githubusercontent.com/u/10100576?v=4" alt="powerline" title="powerline" width="120" height="120"></a>
+
+# FONTS
+
+![[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)]
+Repo by powerline
+Patched fonts for Powerline users.
+
+Installation:
+
+Usage:
+
+Contributors:
+
+Tests:
+
+Questions:
--- index.js
+++ index.js
@@ -6,63 +6,67 @@ const api = require('./api.js')
 const generateMarkdown = require('./generateMarkdown.js')
 
 const writeToFile = (fileName, data) => {
-  fs.writeFile(fileName + '.md', data, error => error ? console.error(error) : console.log(`${fileName + '.md'} generated!`))
+    fs.writeFile(fileName + '.md', data, error => error ? console.error(error) : console.log(`${fileName + '.md'} generated!`))
 }
 
 // API user ID name & repository 
 const init = async _ => {
     let object = {}
     do {
-      const { user, repo } = await prompt([
-        {
-          type: 'input',
-          name: 'user',
-          message: 'What is your GitHub user name?'
-        },
-        {
-          type: 'input',
-          name: 'repo',
-          message: 'What is your repository name?'
+        const { user, repo } = await prompt([
+            {
+                type: 'input',
+                name: 'user',
+                message: 'What is your GitHub user name?'
+            },
+            {
+                type: 'input',
+                name: 'repo',
+                message: 'What is your repository name?'
+            }
+        ])
+
+        object = await api.getUser(user, repo)
+        if (!object) {
+            console.error('Repo not found!')
         }
-      ])
-      object = await api.getUser(user, repo)
-      if (!object) {
-        console.error('Repo not found!')
-      } else {
-        console.log(`${object.fullName} found!`)
-      }
-    } while (!object)
+        else {
+            console.log(`${object.fullName} found!`)
+        }
+    }
+    while (!object)
 
     Object.assign(object, await prompt([
-    
+
         {
-          type: 'input',
-          name: 'usage',
-          message: 'What is the usage description?'
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage description?'
         },
         {
-          type: 'input',
-          name: 'install',
-          message: 'What are the installation instructions?'
+            type: 'input',
+            name: 'install',
+            message: 'What are the installation instructions?'
         },
-       
+
         {
-          type: 'input',
-          name: 'contrib',
-          message: 'Who are the contributors?'
-        },
-        {
-          type: 'input',
-          name: 'test',
-          message: 'What are the tests?'
+            type: 'input',
+            name: 'contrib',
+            message: 'Who are the contributors?'
         },
         {
-          type: 'input',
-          name: 'questions',
-          message: 'Any questions?'
+            type: 'input',
+            name: 'test',
+            message: 'What are the tests?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Any questions?'
         }
-      ]))
-      writeToFile(object.title, await generateMarkdown(object))
-    }
-    
-    init()
+    ]))
+
+    writeToFile(object.title, await generateMarkdown(object))
+}
+
+init()
